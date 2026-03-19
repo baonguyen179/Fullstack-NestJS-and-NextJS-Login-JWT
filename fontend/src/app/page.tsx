553faccb7dc7150eb7c72e8c3b606419c11fbef2
baonguyen179 <baohp17@gmail.com>
2026-03-19 14:14:33 +0700

    Sửa lỗi không vào được thư mục fontend

---
+++ fontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+export default function Home() {
+  return (
+    <div>Hello World!
+    <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+  </div>
+  );
+}
